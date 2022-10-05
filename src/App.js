19672737cc7600ac8a12ed5cdb4e3fdd039a3bb6
codnr <wnd75913@gmail.com>
2022-10-05 14:13:29 +0900

    Chore: localhost 3006으로 변경

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
+const BoxOne = styled.div`
   background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
   width: 100px;
   height: 100px;
 `;
@@ -13,7 +18,8 @@ const Box = styled.div`
 function App() {
   return (
     <Father>
-      <Box />
+      <BoxOne />
+      <BoxTwo />
     </Father>
   );
 }
