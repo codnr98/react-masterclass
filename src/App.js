9883c02e3f902f622dfd54aadf262f4dff1e816a
codnr <wnd75913@gmail.com>
2022-10-05 14:40:00 +0900

    'As' and attrs

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
+const Circle = styled(Box)`
+  border-radius: 50%;
+`;
+
+const Input = styled.input.attrs({ required: true })``;
+
 function App() {
   return (
     <Father>
-      <BoxOne />
-      <BoxTwo />
+      <Box bgColor="tomato" />
+      <Circle bgColor="teal" />
+      <Input />
     </Father>
   );
 }
