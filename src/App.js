f8052af263cc9874269cae23b55f8068853731e2
codnr <wnd75913@gmail.com>
2022-10-05 16:16:20 +0900

    Themes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,20 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
+const Title = styled.span`
+  color: ${(props) => props.theme.textColor};
+`;
 const Wrapper = styled.div`
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const Emoji = styled.span`
-  font-size: 50px;
-`;
-
-const animation = keyframes`
-    from {transform:rotate(0deg);
-      border-radius: 25%;
-    }
-    to {
-      transform:rotate(360deg);
-      border-radius: 50%;
-    }
-  `;
-
-const Box = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${animation} 1s linear infinite;
-
-  ${Emoji} {
-    &:hover {
-      font-size: 70px;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😀</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
