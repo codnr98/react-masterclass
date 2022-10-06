070bcd4f97cf1adb7d6b25284daf91fce55eb4ee
codnr <wnd75913@gmail.com>
2022-10-06 13:07:26 +0900

    Typescript

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
+  return (
+    <Container>
+      <Title>hi</Title>
+    </Container>
+  );
+}
+
+export default App;
