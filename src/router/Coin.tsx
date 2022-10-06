1a63102b79be54744e51107778e71409e9bbc19c
codnr <wnd75913@gmail.com>
2022-10-06 15:09:35 +0900

    Feat: Coin라우터에 current URL 출력

--- src/router/Coin.tsx
+++ src/router/Coin.tsx
@@ -1,5 +1,9 @@
+import { useParams } from "react-router-dom";
+
 function Coin() {
-  return <h1>Coin</h1>;
+  // useParams()는 매치되는 <Route path>에 의해 current URL을 가져온다.
+  const { coinId } = useParams<string>();
+  return <h1>Coin: {coinId}</h1>;
 }
 
 export default Coin;
