cc7c59a72887112319da4076df3db15edb4a9aed
codnr <wnd75913@gmail.com>
2022-10-11 16:56:42 +0900

    interface 생성

--- src/router/Coin.tsx
+++ src/router/Coin.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -23,14 +24,79 @@ const Title = styled.div`
   font-weight: 700;
 `;
 
+const Loading = styled(Title)`
+  margin-top: 5px;
+  font-size: 35px;
+  font-weight: 700;
+`;
+
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  tags: object;
+  team: object;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: object;
+  links_extended: object;
+  whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: object;
+}
+
 function Coin() {
   // useParams()는 매치되는 <Route path>에 의해 current URL을 가져온다.
   // Coins.tsx에서 보내진 state를 기반으로 title을 출력한다.
+  const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState({});
+  const [price, setPrice] = useState({});
+  const { coinId } = useParams();
   const { state } = useLocation();
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      console.log(priceData);
+      setInfo(infoData);
+      setPrice(priceData);
+      setLoading(false);
+    })();
+  }, []);
   return (
     <Container>
       <Wrapper>
-        <Title>{state.name}</Title>
+        {loading ? <Loading>Loading...</Loading> : <Title>{state.name}</Title>}
       </Wrapper>
     </Container>
   );
