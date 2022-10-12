89ea7bfcbc46432838ef19f3b70194dc6e306590
codnr <wnd75913@gmail.com>
2022-10-12 17:18:24 +0900

    Chore: react-query설치

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { Theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}></QueryClientProvider>
     <ThemeProvider theme={Theme}>
       <App />
     </ThemeProvider>
